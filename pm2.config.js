9e920932abbc5f89210866b0b24325d87b2698ad
Ravy <smart.fz@mail.ru>
2019-12-23 05:01:12 +0300

    Add some quests

---
+++ pm2.config.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+  apps: [
+    {
+      name: 'FEBATTLE',
+      script: './ws-server/index.js',
+      instances: 1,
+      exec_mode: 'fork',
+      cwd: './current',
+      error_file: '../logs/pm2.err.log',
+      out_file: '../logs/pm2.out.log',
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+  deploy: {
+    production: {
+      user: 'altcover',
+      host: '45.67.56.165',
+      ref: 'origin/master',
+      repo: 'git@github.com:Rovniy/wg_battle.git',
+      path: '/var/www/febattle/html',
+      'post-deploy': 'yarn && yarn build && pm2 restart all'
+    }
+  }
+}
